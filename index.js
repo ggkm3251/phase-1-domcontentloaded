de1b5daaf605327fea250b9a4155064b9cee556e
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-13 13:24:33 +0300

    commit tree b8073d2827175544b48c6e52baadc3e37c523d9b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+/*document.querySelector( 'p' ).textContent
+
+const event = document.createEvent( 'Event' );
+    event.initEvent( 'DOMContentLoaded', true, true );
+    window.document.dispatchEvent( event )*/
+    
+document.addEventListener( "DOMContentLoaded", function () {
+  changeText()
+} );
+
+
+function changeText() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}    
